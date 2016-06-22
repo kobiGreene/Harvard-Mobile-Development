e38aa8993ec7760ca5cea5ef9e67ee8ee26c08ee
kobiGreene <kobi.greene3@gmail.com>
2016-06-22 18:06:55 -0400

    Work for today

---
+++ Lecture2B/Lecture2B/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lecture2B
+//
+//  Created by Kobi Greene on 6/22/16.
+//  Copyright © 2016 Kobi Greene. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lecture2B/Lecture2B/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lecture2B
+//
+//  Created by Kobi Greene on 6/22/16.
+//  Copyright © 2016 Kobi Greene. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
