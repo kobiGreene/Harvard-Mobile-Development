5f065888bedb926cd24d907644e55c18d954e300
kobiGreene <kobi.greene3@gmail.com>
2016-07-22 15:32:45 -0400

    Complete

--- Assignment5.playground/Contents.swift
+++ Assignment5.playground/Contents.swift
@@ -2,11 +2,12 @@
 // Help From Section
 
 func isLeap(year:Int) -> Bool {
-    return year % 4 == 0 ? year % 100 == 0 ? year % 400 == 0 ? true : false: true : false
+    return year % 4 == 0 ? (year % 100 == 0 ? (year % 400 == 0 ? true : false) : true) : false
 }
 isLeap(1960)
 isLeap(1900)
 isLeap(2000)
+
 var months = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 func julianDate(year: Int, month: Int, day: Int) -> Int{
     let daysInMonth = (0..<month).map({isLeap(year) && $0 == 2 ? 29 : months[$0]}).reduce(0, combine: {$0 + $1})
